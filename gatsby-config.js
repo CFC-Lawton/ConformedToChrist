b3578691b3f5436fda16f4a5e8dbc31530f45329
Aaron Endsley <aaronendsley@gmail.com>
2023-04-05 17:35:31 -0500

    added styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     description: `The Conformed to Christ podcast, where we seek to engage the mind affect the heart, and call people to follow Christ.`,
     author: `Christ's Fellowship Church`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-    siteData: {
+    siteContentData: {
       tagline: `"We Aim To Engage the Mind, Affect The Heart, and Call People to Follow Christ."`,
       mission: `Additionally, our aim is to introduce and explain passages of Scripture and difficult theological doctrines in a down-to-earth and easy-to-grasp manner. Theology and the Bible should impact your life and our goal is that we might play a small part in seeing that happen.`,
       georgeData: {},
@@ -38,6 +38,7 @@ module.exports = {
         id: 'guid',
       },
     },
+    `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,39 @@ import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import { createGlobalStyle } from "styled-components";
+// import "./layout.css"
+
+
+const GlobalStyle = createGlobalStyle`
+   body{
+    --border-radius: 4px;
+    --color-text: #fff;
+    --color-primary: #7026b9;
+    --color-code-bg: #000000;
+    --color-code: #8a6534;
+    --font-sans: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial,
+      sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+    --font-mono: SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono",
+      "Courier New", monospace;
+    --font-lg: 18px;
+    --font-md: 16px;
+    --font-sm: 14px;
+    --font-sx: 12px;
+    --line-height-loose: 1.75;
+    --line-height-normal: 1.5;
+    --line-height-dense: 1.1;
+    --space-1: 4px;
+    --space-2: 8px;
+    --space-3: 16px;
+    --space-4: 24px;
+    --space-5: 32px;
+    --space-6: 64px;
+    --size-content: 54rem;
+    --size-gutter: var(--space-5);
+    --size-gap: var(--space-6);
+  }
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,6 +56,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <GlobalStyle />
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
