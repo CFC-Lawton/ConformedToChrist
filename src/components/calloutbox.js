a26511939232c8bb79befbb1ab5e7d88228cd6a8
Aaron Endsley <aaronendsley@gmail.com>
2023-04-12 17:28:15 -0500

    fixed a npm package vulnerability and styled the header text added the calloutbox component file

---
+++ src/components/calloutbox.js
@@ -0,0 +1,3 @@
+import * as React from 'react';
+
+//This Component will be a reusable component that allows text data to be taken in and create a callout box with the correct styling dictated in the mockup 
