f26308f34c8fe6eeeb60f9366b25f1c96196a133
Aaron Endsley <aaronendsley@gmail.com>
2023-06-01 09:03:05 -0500

    working on Host Component rendering based on direction of content

--- src/components/host.js
+++ src/components/host.js
@@ -5,7 +5,11 @@ import { GatsbyImage } from 'gatsby-plugin-image';
 
 // create second host container with a row reverse on it and render out a new component based off of flow direction key
 
-const HostContainer = styled.div`
+const HostWrapper = styled.div`
+    width:100%;
+`
+
+const HostContainerLeft = styled.div`
  width:100%;
  display:flex;
  flex-direction: column;
@@ -14,6 +18,15 @@ const HostContainer = styled.div`
  } 
 `;
 
+
+const HostContainerRight = styled.div`
+width:100%;
+ display:flex;
+ flex-direction: column;
+ @media (min-width: 1000px){
+    flex-direction: row-reverse; 
+ }
+`
 
 const HostImageWrap = styled.div`
     width:80%;
@@ -71,32 +84,67 @@ const HostName = styled.div`
 `;
 
 
+function HostLeft({ image, bioContent }) {
+    <HostContainerLeft>
+        <HostImageContainer>
+            <HostImageWrap>
+                <HostImage>
+                    <GatsbyImage image={image[0].node.childrenImageSharp[0].gatsbyImageData} alt={bioContent.name} />
+                </HostImage>
+            </HostImageWrap>
+            <HostDecorator />
+        </HostImageContainer>
+        <HostDescription>
+            <HostName>
+                <h2>{bioContent.name}</h2>
+            </HostName>
+            <div>
+                {bioContent.bio.map((sentence, i) => <p key={`${bioContent.name.split(' ').join('')}${i}`}>{sentence}</p>)}
+            </div>
+        </HostDescription>
+    </HostContainerLeft>
+}
+
+function HostRight({ image, bioContent }) {
+    <HostContainerRight>
+        <HostImageContainer>
+            <HostImageWrap>
+                <HostImage>
+                    <GatsbyImage image={image[0].node.childrenImageSharp[0].gatsbyImageData} alt={bioContent.name} />
+                </HostImage>
+            </HostImageWrap>
+            <HostDecorator />
+        </HostImageContainer>
+        <HostDescription>
+            <HostName>
+                <h2>{bioContent.name}</h2>
+            </HostName>
+            <div>
+                {bioContent.bio.map((sentence, i) => <p key={`${bioContent.name.split(' ').join('')}${i}`}>{sentence}</p>)}
+            </div>
+        </HostDescription>
+    </HostContainerRight>
+}
+
+function HostComponent({ image, flowDirection, bioContent }) {
+    let direction = flowDirection.toLowerCase();
+
+    if (direction = 'left') {
+        return <HostLeft image={image} bioContent={bioContent} />
+    } else if (direction = 'right') {
+        return <HostRight image={image} bioContent={bioContent} />
+    } else {
+        console.log('lol')
+    }
 
 
+}
 
-/// look into gatsbyImage component
+//Find out why the components will not render
 export default function Host({ image, flowDirection, bioContent }) {
-    console.log(flowDirection, bioContent);
-
-
     return (
-        <HostContainer>
-            <HostImageContainer>
-                <HostImageWrap>
-                    <HostImage>
-                        <GatsbyImage image={image[0].node.childrenImageSharp[0].gatsbyImageData} alt={bioContent.name} />
-                    </HostImage>
-                </HostImageWrap>
-                <HostDecorator />
-            </HostImageContainer>
-            <HostDescription>
-                <HostName>
-                    <h2>{bioContent.name}</h2>
-                </HostName>
-                <div>
-                    {bioContent.bio.map((sentence, i) => <p key={`${bioContent.name.split(' ').join('')}${i}`}>{sentence}</p>)}
-                </div>
-            </HostDescription>
-        </HostContainer>
+        <HostWrapper>
+            {/* <HostComponent image={image} flowDirection={flowDirection} bioContent={bioContent} /> */}
+        </HostWrapper>
     )
 }
