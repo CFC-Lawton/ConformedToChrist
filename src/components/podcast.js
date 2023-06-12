f0bda7d551bf424f3174744fcde58db69bfd8c65
Aaron Endsley <aaronendsley@gmail.com>
2023-06-12 17:59:19 -0500

    styled and built out more of the podcast component

--- src/components/podcast.js
+++ src/components/podcast.js
@@ -1,23 +1,41 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-const PodcastWrapper = styled.div`
+const PodcastsWrapper = styled.div`
     display:grid;
     width: 80%;
     grid-template-columns: 1fr;
     margin: 0 auto;
+    
     @media (min-width:700px){
         width:70%;
         grid-template-columns: 1fr 1fr;
+        
     }
     @media (min-width:1000px){
         grid-template-columns: 1fr 1fr 1fr;
         width:100%;
     }
-
-
 `;
 
+const Podcast = styled.div`
+    width: 80%;
+
+    img{
+        width:100%;
+        border-bottom: solid 10px var(--c2c-red);
+        border-radius:5px;
+
+    }
+
+    h3{
+        color:#fff;
+    }
+
+    p{}
+
+    
+`;
 
 
 
@@ -25,8 +43,16 @@ const PodcastWrapper = styled.div`
 export default function Podcasts({podcasts}){
     console.log(podcasts);
     return (
-        <PodcastWrapper>
-            {podcasts.map((podcast)=><p>{podcast.node.item.title}</p>)}
-        </PodcastWrapper>
+        <PodcastsWrapper>
+            {podcasts.map((podcast)=>{
+                return(
+                    <Podcast key={podcast.node.id}>
+                        <img src={podcast.node.item.itunes.image}/>
+                        <h3>{podcast.node.item.title}</h3>
+                        <p>{`${podcast.node.item.content.split('>')[1].split('<')[0].substring(0, 200)}...`}</p>
+                    </Podcast>
+                )
+            })}
+        </PodcastsWrapper>
     )
 }
