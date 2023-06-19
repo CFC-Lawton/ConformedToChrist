df2beb00fce00b278ced7a35509c90108412cc3f
Aaron Endsley <aaronendsley@gmail.com>
2023-06-19 14:38:46 -0500

    added the podcast summary to the page

--- src/templates/podcast.js
+++ src/templates/podcast.js
@@ -1,11 +1,26 @@
 import * as React from 'react';
 import {graphql} from 'gatsby';
 import styled from 'styled-components';
+import Layout from '../components/layout';
+
+
+
+  
+
 
 export default function Podcast({data}){
+  function createPodContent(html){
+    return {__html:html};
+  }
+
   console.log(data)
     return(
-        <div>{data.podcastRssFeedEpisode.item.title}</div>
+      <Layout>
+        
+          <h1>{`Episode ${data.podcastRssFeedEpisode.item.itunes.episode}: ${data.podcastRssFeedEpisode.item.title}`}</h1>
+          <div dangerouslySetInnerHTML={createPodContent(data.podcastRssFeedEpisode.item.content)}/>
+          
+        </Layout>
     )
 }
 
@@ -16,9 +31,10 @@ query($id: String!){
       content
     }
     item {
+      content
       itunes {
         episode
-        summary
+        
       }
       title
       enclosure {
